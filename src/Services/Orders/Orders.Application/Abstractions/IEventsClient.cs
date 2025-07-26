f7aa880f1ea717379566e63ee0ffb56a26ead971
hopdinhp02 <hopdinhp02@gmail.com>
2025-07-26 19:10:10 +0700

    add resilience to the inter-service communication

---
+++ src/Services/Orders/Orders.Application/Abstractions/IEventsClient.cs
@@ -0,0 +1,13 @@
+﻿namespace TicketFlow.Orders.Application.Abstractions
+{
+    public interface IEventsClient
+    {
+        Task<TicketAvailabilityDto?> GetTicketAvailabilityAsync(
+            Guid eventId,
+            Guid ticketTypeId,
+            CancellationToken cancellationToken = default
+        );
+    }
+
+    public record TicketAvailabilityDto(Guid TicketTypeId, int AvailableQuantity);
+}
