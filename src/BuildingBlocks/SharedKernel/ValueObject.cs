5f8b3fc4e01678556c359935c4a80b83960019c1
hopdinhp02 <hopdinhp02@gmail.com>
2025-12-19 22:19:45 +0700

    refactor Entity + ValueObject

--- src/BuildingBlocks/SharedKernel/ValueObject.cs
+++ src/BuildingBlocks/SharedKernel/ValueObject.cs
@@ -45,9 +45,12 @@
 
         public override int GetHashCode()
         {
-            return GetEqualityComponents()
-                .Select(x => x?.GetHashCode() ?? 0)
-                .Aggregate((x, y) => x ^ y);
+            var hash = new HashCode();
+            foreach (var component in GetEqualityComponents())
+            {
+                hash.Add(component);
+            }
+            return hash.ToHashCode();
         }
     }
 }
